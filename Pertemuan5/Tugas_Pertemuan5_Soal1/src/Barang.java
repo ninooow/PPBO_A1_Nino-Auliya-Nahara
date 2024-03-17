bd551c993c109f006166178226fce6d23aa6350c
ninooow <ninoauliyanahara@gmail.com>
2024-03-17 23:29:56 +0700

    yow

---
+++ Pertemuan5/Tugas_Pertemuan5_Soal1/src/Barang.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Barang {
+    private String namaBarang;
+    private int hargaBarang, jumlahBarang;
+
+    public Barang(String namaBarang, int hargaBarang, int jumlahBarang) {
+        this.namaBarang = namaBarang;
+        this.hargaBarang = hargaBarang;
+        this.jumlahBarang = jumlahBarang;
+    }
+
+//    public void setNamaBarang(String namaBarang) {
+//        this.namaBarang = namaBarang;
+//    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+//    public void setHargaBarang(int hargaBarang) {
+//        this.hargaBarang = hargaBarang;
+//    }
+
+    public int getHargaBarang() {
+        return hargaBarang;
+    }
+
+//    public void setJumlahBarang(int jumlahBarang) {
+//        this.jumlahBarang = jumlahBarang;
+//    }
+
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+}
